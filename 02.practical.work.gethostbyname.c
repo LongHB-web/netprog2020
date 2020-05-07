a41da77ccd58c115163c1bf7dcb51c32ab921fd6
LongHB-web <longhb.bi9147@st.usth.edu.vn>
2020-05-07 09:45:39 +0700

    lapwork3-Networkprograming

--- 02.practical.work.gethostbyname.c
+++ 02.practical.work.gethostbyname.c
@@ -6,7 +6,7 @@
 #include <sys/socket.h> 
 #include <netinet/in.h> 
 #include <arpa/inet.h>
-void checkHostName(ins hostname){
+void checkHostName(int hostname){
     if (hostname == -1){
         perror("gethostname");
         exit(1);
@@ -16,6 +16,7 @@ void checkHostEntry(struct hostent * hostentry){
     if(hostentry == NULL){
         perror("gethostbyname");
         exit(1);
+        
     }
 }
 int main(){
@@ -24,10 +25,10 @@ char *IPbuffer;
 struct hostent *host_entry;
 int hostmain;
 
-hostname = gethostname(hostbuffer, sizeof(hostbuffer));
-checkHostName(host_entry);
+hostname = gethostbyname(hostbuffer, sizeof(hostbuffer));
+checkHostbyName(host_entry);
 
-IPbuffer =  intet_ntoa (*((struct in_addr*)host_entry->h_addr_list[0]));
+IPbuffer =  inet_ntoa (*((struct in_addr*)host_entry->h_addr_list[0]));
 printf("hostname: %s\n", hostbuffer);
 printf("Host IP: %s", IPbuffer);
 return 0;
