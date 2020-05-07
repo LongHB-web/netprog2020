03c82c0e8a7f7b34cdee0378d2e9940f122b3284
LongHB-web <longhb.bi9147@st.usth.edu.vn>
2020-05-07 09:35:20 +0700

    Labwork3-Networkprograming

---
+++ 03.practical.work.server.setup.c
@@ -0,0 +1,42 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    int ss, cli, pid;
+    struct sockaddr_in ad;
+    char s[100];
+    socklen_t ad_length = sizeof(ad);
+
+    ss = socket(AF_INET, SOCK_STREAM, 0);
+
+    memset(&ad, 0, sizeof(ad));
+    ad.sin_family = AF_INET;
+    ad.sin_addr.s_addr = INADDR_ANY;
+    ad.sin_port = htons(8784);
+    if (bind(ss, (struct sockaddr *)&ad, ad_length) < 0) {
+    	printf("Can't bind");
+	exit(1);
+    }
+
+    if (listen(ss, 0) == -1){
+    	printf("Can't listen on port 8784");
+	exit(1);
+    }
+
+    cli = accept(ss, (struct sockaddr *)&ad, &ad_length);
+    if (cli == -1) {
+    	printf("Something wrong");
+	exit(1);
+    } else 
+	printf("Connected");
+
+    // disconnect
+    close(cli);
+}
